aa7df68205073ca75511a5b0c14abcf955ace0a9
kuryx <130793957+kuryx@users.noreply.github.com>
2023-09-01 19:57:22 -0500

    Commit Services Modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,33 @@ tabs.forEach(tab =>
 
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+        modalBtns = document.querySelectorAll('.services__button'),
+        modalCloses = document.querySelectorAll('.services__modal-close')
 
+let modal = function (modalclick)
+{
+    modalViews[modalclick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) =>
+{
+    modalBtn.addEventListener('click', () =>
+    {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) =>
+{
+    modalClose.addEventListener('click', () =>
+    {
+        modalViews.forEach((modalView) =>
+        {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
